654b1e641fc9bf3c381b80a630a4989f6d6cbcf9
KRISHNAHARI0618 <harivardhan9397921834@gmail.com>
2025-05-10 15:44:13 +0530

    10-05-2025-05m-1746872035s

--- Session-01/main.tf
+++ Session-01/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.97.0"
+    }
+
+  }
+  backend "s3" {
+    bucket = "${local.name}"
+    
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -6,6 +20,5 @@ resource "aws_instance" "web1" {
   count         = 2
   ami           = "ami-084568db4383264d4"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.publicSubnet.id
-
 }
+
